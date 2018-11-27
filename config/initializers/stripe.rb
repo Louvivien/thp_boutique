2ccfb8720333cfcf82d8aa559a01937c2a2e5e08
Colin Lecordier <colin.lecordier@gmail.com>
2018-11-27 17:49:31 +0100

    stripe add

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,6 @@
+Rails.configuration.stripe = {
+  :publishable_key => Rails.application.credentials.dig(:PUBLISHABLE_KEY),
+  :secret_key      => Rails.application.credentials.dig(:SECRET_KEY)
+}
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
