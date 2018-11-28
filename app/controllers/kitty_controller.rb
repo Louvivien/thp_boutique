67d465d2a10a6c0c264abfed71d9148449b36e93
Colin Lecordier <colin.lecordier@gmail.com>
2018-11-28 22:10:01 +0100

    schema colin

--- app/controllers/kitty_controller.rb
+++ app/controllers/kitty_controller.rb
@@ -1,34 +1,27 @@
 class KittyController < ApplicationController
-	@@num = 0
 
-	def index
-		if current_user && current_user.panier == nil
-			current_user.panier = Panier.new
-		end
-    end
+  def index
+    if current_user && current_user.panier == nil
+	    current_user.panier = Panier.new
+	  end
+  end
 	
-	 def show
-		 @@num = params[:id].to_i
-                 @item = Item.find(params[:id].to_i)
-                 if current_user
-                 @current_user = User.find(current_user.id)
-                 end
-        end
-
-	 def create
-		@item = Item.find(@@num)
-		current_user.panier.items << @item
-		redirect_to '/'
-	 end
-
-    def new
-    	@item2 = Item.all
+	def show
+	  @@num = params[:id].to_i
+    @item = Item.find(params[:id].to_i)
+    if current_user
+      @current_user = User.find(current_user.id)
     end
+  end
 
-    def addpanier
-    	@item = Item.find(@@num)
-		current_user.panier.items << @item
-		p "ajout au panier"
-    end
+  def create
+    @item = Item.find(@@num)
+    current_user.panier.items << @item
+    puts 'coucou'
+    redirect_to '/'
+  end
 
+  def new
+  	@item2 = Item.all
+  end
 end
