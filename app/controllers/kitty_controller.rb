d28df7c2273ab6458daeefbc705745b12d2a3463
bsteve456 <stevealan1@protonmail.com>
2018-11-29 00:49:14 +0100

    add page panier and delete button and total price of the items

--- app/controllers/kitty_controller.rb
+++ app/controllers/kitty_controller.rb
@@ -1,27 +1,40 @@
 class KittyController < ApplicationController
-
-  def index
-    if current_user && current_user.panier == nil
-	    current_user.panier = Panier.new
-	  end
-  end
+	
+	def index
+		if current_user && current_user.panier == nil
+			current_user.panier = Panier.new
+		end
+	end
 	
 	def show
-	  @@num = params[:id].to_i
-    @item = Item.find(params[:id].to_i)
-    if current_user
-      @current_user = User.find(current_user.id)
-    end
-  end
+		@@num = params[:id].to_i
+		@item = Item.find(params[:id].to_i)
+		if current_user
+			@current_user = User.find(current_user.id)
+		end
+	end
 
-  def create
-    @item = Item.find(@@num)
-    current_user.panier.items << @item
-    puts 'coucou'
-    redirect_to '/'
-  end
+	def create
+		@item = Item.find(@@num)
+		current_user.panier.items << @item
+		puts 'coucou'
+		redirect_to '/'
+	end
+	
+	def new
+		@item2 = Item.all
+	end
 
-  def new
-  	@item2 = Item.all
-  end
+	def panier
+		@price = 0
+		current_user.panier.items.each do |items|
+			 prix = items.price.to_f
+			 @price = @price + prix
+		end
+	end
+
+	def items
+		current_user.panier.items.delete(params[:id].to_i)
+	end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -20,8 +20,10 @@ ActiveRecord::Schema.define(version: 2018_11_28_190143) do
     t.text "description"
     t.string "price"
     t.string "image_url"
+    t.bigint "panier_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["panier_id"], name: "index_items_on_panier_id"
   end
 
   create_table "paniers", force: :cascade do |t|
