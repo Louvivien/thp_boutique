29fe53100d5895de98ffad74a3a59f6657667b31
Colin Lecordier <colin.lecordier@gmail.com>
2018-11-29 12:12:59 +0100

    mailer works

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
-  def welcome_email()
-     mail(from: "me@mailjet.com", to: "you@mailjet.com",
+  def welcome_email(user)
+    @user = user
+     mail(from: "colin.lecordier@gmail.com", to: @user.email,
           subject: "This is a nice welcome email")
    end
 end
