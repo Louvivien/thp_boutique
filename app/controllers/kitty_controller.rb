85d53fad56733cd7cf80975a1fc9c92272138761
bsteve456 <stevealan1@protonmail.com>
2018-11-29 16:24:30 +0100

    add order and finish panier

--- app/controllers/kitty_controller.rb
+++ app/controllers/kitty_controller.rb
@@ -1,5 +1,23 @@
 class KittyController < ApplicationController
 
+  skip_before_action :verify_authenticity_token
+
+	
+  def duplicate(product)
+	  x = 0
+	  Item.all.each do |items|
+		if (items.title == product.title)
+			x = x + 1
+		end
+	  end
+
+	  if (x >= 2 && product.id > 20)
+		  return true
+	  else
+		  return false
+	  end
+  end
+
   def index
 	if current_user && current_user.panier == nil
 		current_user.panier = Panier.new
@@ -14,15 +32,33 @@ class KittyController < ApplicationController
 	end
   end
 
-  def create
-    @item = Item.find(@@num)
-	current_user.panier.items << @item
-	puts 'coucou'
-	redirect_to '/'
-  end
+  #def create
+  # @item = Item.find(@@num)
+  # current_user.panier.items << @item
+  # puts 'coucou'
+  # redirect_to '/'
+  # end
 	
   def new
-	@item2 = Item.all
+	  @array = Array.new
+	  Item.all.each do |item|
+		  if (duplicate(item) == false)
+			  @array << item
+		  end
+	  end
+	  @item2 = Item.all
+  end
+
+  def product
+	i = 0
+
+	while(i < params[:number].to_i)
+		put_item = Item.find(params[:id].to_i)
+		put_item_duplicate = put_item.dup
+		current_user.panier.items << put_item_duplicate
+		i = i + 1
+	end
+	redirect_to '/index'
   end
 
   def panier
@@ -43,7 +79,18 @@ class KittyController < ApplicationController
   end
   
   def items
-	current_user.panier.items.delete(params[:id].to_i)
-	redirect_to '/panier'
+	  current_user.panier.items.delete(params[:id].to_i)
+	  redirect_to '/panier'
+  end
+
+  def order
+	  current_user.orders << Order.new
+	  order = current_user.orders.last
+	  panier = current_user.panier.items
+	  panier.each do |item|
+		  order.items << item
+		  current_user.panier.items.delete(item.id)
+	  end
+	  redirect_to '/'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,14 @@ Rails.application.routes.draw do
   post '/index', to: 'kitty#create'
   get '/index', to: 'kitty#new'
   get '/panier', to:'kitty#panier'
+  post '/panier', to: 'kitty#order'
   delete '/panier', to: 'kitty#items'
   devise_for :users
   resources :charges
   root 'kitty#index'
+  post '/kitty/:id', to: 'kitty#product'
   post '/kitty/new' , to: 'kitty#create'
   resources :kitty
+
 
 end
