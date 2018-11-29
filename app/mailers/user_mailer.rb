9a35b6da394186bc8b313ff420c1f970bcda47ca
Colin Lecordier <colin.lecordier@gmail.com>
2018-11-29 23:52:48 +0100

    mailer add to order

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,4 +4,10 @@ class UserMailer < ApplicationMailer
     mail(from: "colin.lecordier@gmail.com", to: @mail_user,
         subject: "Bienvenue sur ce site de chaton")
    end
+
+   def client_order(mail_user)
+   	@mail_user = mail_user
+    mail(from: "colin.lecordier@gmail.com", to: @mail_user,
+        subject: "merci pour votre commande")
+   end
 end
