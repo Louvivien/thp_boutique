7cac8e163e0c9f239e042d04eb76679c66a83856
Colin Lecordier <colin.lecordier@gmail.com>
2018-11-29 20:50:50 +0100

    stripe add to order

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,10 +1,31 @@
 class ChargesController < ApplicationController
 def new
+  @current_user = User.find(current_user.id)
+  @price_total = 0
+  @current_user.orders.last.items.each do |items|
+    items.price.sub!(",", ".")
+    prix = items.price.to_f
+    @price_total = @price_total + prix
+  end
+  @amount = @price_total * 100
+  @amount = @amount.to_i
 end
 
 def create
   # Amount in cents
-  @amount = 100
+  @current_user = User.find(current_user.id)
+  @price_total = 0
+  @current_user.orders.last.items.each do |items|
+    items.price.sub!(",", ".")
+    prix = items.price.to_f
+    @price_total = @price_total + prix
+  end
+  puts @price_total.class 
+  @amount = @price_total * 100
+  @amount = @amount.to_i
+  puts @amount
+  puts @amount.class
+  puts 'coucou'
 
   customer = Stripe::Customer.create(
     :email => params[:stripeEmail],
